278dd42706cb749eb60bf6bca8605347e78616a9
Xiong (Shawn) He <sxuxxh@yahoo.com>
2022-06-11 15:29:02 -0400

    add login front end

--- payroll-frontend/src/app/app-routing.module.ts
+++ payroll-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LoginsuccesslandingComponent } from './loginsuccesslanding/loginsuccesslanding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'loginsuccesslanding', component:LoginsuccesslandingComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ payroll-frontend/src/app/loginsuccesslanding/loginsuccesslanding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginsuccesslanding',
+  templateUrl: './loginsuccesslanding.component.html',
+  styleUrls: ['./loginsuccesslanding.component.css']
+})
+export class LoginsuccesslandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
